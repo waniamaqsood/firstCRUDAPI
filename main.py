ea7af9dec6d6c72cb5af0536c18b880bff818364
waniamaqsood <waniamaqsoood@gmail.com>
2026-08-14 09:44:59 +0500

    Adding API's endpoint

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from typing import List
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI CRUD API",
+    description="A simple CRUD API",
+    version="1.0.0"
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    id: int
+    title: str 
+    done: bool
+
+class ItemCreate(BaseModel):
+    title: str 
+    done: bool
+
+task_db = [
+    {"id": 1, "title": "Task One", "done": True},
+    {"id": 2, "title": "Task Two", "done": False},
+    {"id": 3, "title": "Task Three", "done": True},
+]
+
+@app.get("/") 
+def read_root():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/api/tasks"] }
